1de4db4ad4961bebe3f5a41aaa53d2639c6fbba0
GiulianaMiret <dariela_77@hotmail.com>
2015-02-11 22:34:46 -0300

    Arboles
    Aca intente implementar los AVL.. pero esta RE mal porque ni andan las
    cosas feas estas.. y me re perdi.. asique debe ser cualquiera

--- Arbol.pas
+++ Arbol.pas
@@ -1,80 +1,42 @@
 Unit Arbol;
-
-interface
-uses Crt, Menu, Tipos, Archivos;
 		
-	procedure crearArbol (var A:arbolArt; var B:arbolArt);
-	function arbolVacio (A:arbolArt): boolean;
-	procedure insertarArbol (var A:arbolArt; nodo: Art; var B: arbolArt);
-	procedure insertarA (var A: arbolArt; nodo: Art);
-	procedure insertarB (var B: arbolArt; nodo: Art);
-	procedure buscarCodigo (A: arbolArt; buscado: word; var nodo: Art; var salir: boolean);
-	procedure buscarDesc (B: arbolArt; bus:string; var nodo: Art; var salir: boolean);
-	procedure cargarArbol (var A:arbolArt; var B:arbolArt; var arA: ArchivoArt);
+interface
+uses Crt, Menu, Tipos, Archivo, ProArbol;
+		
+	procedure crearArbol (var A:arbolArtC; var B:arbolArtD);
+	function arbolVacioA (A:arbolArtC): boolean;
+	function arbolVacioB (B:arbolArtD):boolean;
+	procedure cargarArbol (var A:arbolArtC; var B:arbolArtD; var arA: ArchivoArt);
+	Procedure insertarArbol(Var A:arbolArtC ;var B:arbolArtD ; nodoC: ArtC; nodoD:ArtD);
+	procedure buscarCodigo (A: arbolArtC; buscado: word; var nodoC: ArtC; var salir: boolean);
+	procedure buscarDesc (B: arbolArtD; bus:string; var nodoD: ArtD; var salir: boolean);
+	Procedure Borrar (var A: arbolArtC; var B:arbolArtD; nodoC: ArtC; nodoD: ArtD);
 
 implementation
 
 	
-	procedure crearArbol (var A:arbolArt; var B:arbolArt);
+	procedure crearArbol (var A:arbolArtC; var B:arbolArtD);
 	Begin
 		A:= nil;
 		B:= nil;
 	end;
 	
-	function arbolVacio (A:arbolArt):boolean;
+	function arbolVacioA (A:arbolArtC):boolean;
 	Begin
-		arbolVacio := (A=nil);
+		arbolVacioA := (A=nil);
 	end;
 	
-	procedure insertarArbol (var A:arbolArt; nodo: Art; var B: arbolArt);
+	function arbolVacioB (B:arbolArtD):boolean;
 	Begin
-		insertarA (A, nodo);
-		insertarB (B, nodo);
+		arbolVacioB := (B=nil);
 	end;
 	
-	procedure insertarA (var A: arbolArt; nodo: Art);
-	begin
-		if (A=nil) then
-			begin
-				new(A);
-				A^.info:= nodo;
-				A^.der:=nil;
-				A^.izq:=nil;
-				exit;
-			end
-		else
-		Begin
-			if A^.info.codigo > nodo.codigo then
-				insertarA (A^.izq, nodo)
-			else
-				insertarA (A^.der, nodo);
-		end;
-	end;
-	
-	procedure insertarB (var B: arbolArt; nodo: Art);
-	begin
-		if (B=nil) then
-			begin
-				new(B);
-				B^.info:= nodo;
-				B^.der:=nil;
-				B^.izq:=nil;
-				exit;
-			end
-		else
-		Begin
-			if B^.info.descri > nodo.descri then
-				insertarB (B^.izq, nodo)
-			else
-				insertarB (B^.der, nodo);
-		end;
-	end;
-	
-		procedure cargarArbol (var A:arbolArt; var B:arbolArt; var arA: ArchivoArt);
+	procedure cargarArbol (var A:arbolArtC; var B:arbolArtD; var arA: ArchivoArt);
 	var
 		I: word;
 		pos: word;
-		nodo: Art;
+		nodoC: ArtC;
+		nodoD: ArtD;
 		datoA: tipoArt;
 	begin
 		crearArbol (A, B);
@@ -82,34 +44,43 @@ implementation
 		I:=0;
 		repeat
 			leerArt(arA, datoA, I); //archivo
-			nodo.codigo:= datoA.codigo;
-			nodo.descri:= datoA.descri;
-			nodo.pos:= I;
-			insertarArbol (A, nodo, B);
+			nodoC.codigo:= datoA.codigo;
+			nodoD.descri:= datoA.descri;
+			nodoC.pos:= I;
+			nodoD.pos:= I;
+			insertarArbol(A, B, nodoC, nodoD);
 			inc(I);
 		until (I=pos);
 	end;
-
 	
-	procedure buscarCodigo (A: arbolArt; buscado: word; var nodo: Art; var salir: boolean);
+	Procedure insertarArbol(Var A:arbolArtC ;var B:arbolArtD ; nodoC: ArtC; nodoD:ArtD);
+	Var
+		ok :Boolean;
+	begin
+		ok:= false;
+		insertarA(A, nodoC, ok);
+		insertarB(B, nodoD, ok);
+	end;
+	
+	procedure buscarCodigo (A: arbolArtC; buscado: word; var nodoC: ArtC; var salir: boolean);
 	var
 		j:word;
-		aux:arbolArt;
+		aux:arbolArtC;
 	Begin
 		salir:=false;
-		if not arbolVacio(A) then
+		if not arbolVacioA(A) then
 		Begin
 			aux:=A;
 			if aux^.info.codigo = buscado then
 				begin
-				nodo:= aux^.info
+				nodoC:= aux^.info
 				end
 			else
 			Begin
 				if aux^.info.codigo > buscado then
-					buscarCodigo(aux^.izq, buscado, nodo, salir)
+					buscarCodigo(aux^.izq, buscado, nodoC, salir)
 				else
-					buscarCodigo(aux^.der, buscado, nodo, salir);
+					buscarCodigo(aux^.der, buscado, nodoC, salir);
 			end;
 		end
 		else
@@ -121,29 +92,29 @@ implementation
 			begin
 				repetir; //menu
 				read(buscado);
-				buscarCodigo(A, buscado, nodo, salir)
+				buscarCodigo(A, buscado, nodoC, salir)
 			end
 			else salir:=true;
 		end
 	end;
 	
-	procedure buscarDesc (B: arbolArt; bus:string; var nodo: Art; var salir: boolean);
+	procedure buscarDesc (B: arbolArtD; bus:string; var nodoD: ArtD; var salir: boolean);
 	var
 		j:word;
-		aux:arbolArt;
+		aux:arbolArtD;
 	Begin
 		salir:=false;
-		if not arbolVacio(B) then
+		if not arbolVacioB(B) then
 		Begin
 			aux:=B;
 			if B^.info.descri = bus then
-				nodo:= aux^.info
+				nodoD:= aux^.info
 			else
 			Begin
 				if aux^.info.descri > bus then
-					buscarDesc(aux^.izq, bus, nodo, salir)
+					buscarDesc(aux^.izq, bus, nodoD, salir)
 				else
-					buscarDesc(aux^.der, bus, nodo, salir);
+					buscarDesc(aux^.der, bus, nodoD, salir);
 			end;
 		end
 		else
@@ -155,10 +126,18 @@ implementation
 			begin
 				repetir; //menu
 				readln(bus);
-				buscarDesc(B, bus, nodo, salir)
+				buscarDesc(B, bus, nodoD, salir)
 			end
 			else salir:=true;
 		end
 	end;
+
+	Procedure Borrar (var A: arbolArtC; var B:arbolArtD; nodoC: ArtC; nodoD: ArtD);
+        var ok: boolean;
+        Begin
+		ok := False;
+		BorrarArbolA(A, nodoC, ok);
+		BorrarArbolD (B, nodoD, ok);
+	End;
 	
 End.
